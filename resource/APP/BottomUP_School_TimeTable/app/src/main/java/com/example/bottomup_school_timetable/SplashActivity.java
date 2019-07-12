967c6da32ca609d9da3c9fd60d38f768b51d0d6a
ire4564 <ire4564@gmail.com>
2019-07-12 15:18:04 +0900

    push XML files

---
+++ resource/APP/BottomUP_School_TimeTable/app/src/main/java/com/example/bottomup_school_timetable/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.bottomup_school_timetable;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new splashhandler(), 4000);
+    }
+
+    private class splashhandler implements Runnable {
+        public void run() {
+            startActivity(new Intent(getApplication(), MainActivity.class)); //로딩후 페이지 이동
+            SplashActivity.this.finish(); //로딩페이지 activity stack에서 제거
+        }
+    }
+    @Override
+    public void onBackPressed() {}
+    //초반 화면에서 버튼 못 누르게 설정
+}
