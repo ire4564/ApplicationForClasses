daf35b3b43cda9b68906169e584ea2223c3d436a
Hyunjun Lee <zxzxs9182@gmail.com>
2019-07-29 17:57:12 +0900

    add variable about time

--- ApplicateClass/app/src/main/java/com/example/applicateclass/TimeTable/CustomTimeset.java
+++ ApplicateClass/app/src/main/java/com/example/applicateclass/TimeTable/CustomTimeset.java
@@ -4,12 +4,16 @@ public class CustomTimeset {
     private int day;
     private int startTime;
     private int endTime;
+    private String classname;
 
 
-    public CustomTimeset(int day, int startTime, int endTime) {
+    public CustomTimeset(){}
+
+    public CustomTimeset(int day, int startTime, int endTime, String classname) {
         this.day = day;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.classname = classname;
     }
 
     public void setDay(int day) {
@@ -34,5 +38,13 @@ public class CustomTimeset {
 
     public int getEndTime() {
         return endTime;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname;
     }
 }
