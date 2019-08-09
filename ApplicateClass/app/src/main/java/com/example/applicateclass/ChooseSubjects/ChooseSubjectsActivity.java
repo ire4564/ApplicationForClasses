486ec3da16ba67749238a39532e604b648be9f8d
Hyunjun Lee <zxzxs9182@gmail.com>
2019-08-09 18:43:01 +0900

    Add Check Data

--- ApplicateClass/app/src/main/java/com/example/applicateclass/ChooseSubjects/ChooseSubjectsActivity.java
+++ ApplicateClass/app/src/main/java/com/example/applicateclass/ChooseSubjects/ChooseSubjectsActivity.java
@@ -1,6 +1,7 @@
 package com.example.applicateclass.ChooseSubjects;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.SparseBooleanArray;
@@ -9,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -16,7 +18,17 @@ import com.example.applicateclass.CustomView.CustomSelectBtn;
 import com.example.applicateclass.HowMuchActivity;
 import com.example.applicateclass.R;
 import com.example.applicateclass.SelectTimeSetActivity;
+import com.example.applicateclass.TimeTable.CustomScheduleItem;
+import com.example.applicateclass.TimeTable.CustomTimeTable;
+import com.example.applicateclass.TimeTable.CustomTimeset;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,28 +36,30 @@ public class ChooseSubjectsActivity extends AppCompatActivity {
     public final String PREFERENCE = "com.example.applicateclass"; //저장, 불러오기 위한
     private boolean isBoolean = false;
     int Write, Grade;
-    int index = 0;
-    List<String> subject = new ArrayList<>(); //저장되어 다음으로 넘겨질 정보들
-    final String[] subject_select = {"웹 프로그래밍","컴퓨터 프로그래밍3","알고리즘","창의 소프트웨어 설계","모바일 소프트웨어 설계",
-                                    "인공지능", "선형대수학", "객체지향 설계", "컴퓨터 구조"}; //2번 String 타입의 배열 선언
+    List<CustomScheduleItem> subject = new ArrayList<>(); //저장되어 다음으로 넘겨질 정보들
+    /*final String[] subject_select = {"웹 프로그래밍","컴퓨터 프로그래밍3","알고리즘","창의 소프트웨어 설계","모바일 소프트웨어 설계",
+                                    "인공지능", "선형대수학", "객체지향 설계", "컴퓨터 구조"};*/ //2번 String 타입의 배열 선언
+    String[] subject_select = {};
+    ArrayAdapter adapter;
+    ListView listview;
+    List<CustomScheduleItem> all_subject = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_subjects);//화면 로드
-
         /**********Write는 학점으로 Int형으로 표현하여 정보를 저장한다*******************/
 
         Intent intent_info = getIntent(); //데이터 수신 (학년+ 학점) //다음 액티비티에도 포함하여 저장
         Write = intent_info.getExtras().getInt("Write"); //입력한 학점 받아옴
         Grade= intent_info.getExtras().getInt("Grade"); //선택한 grade1, grade2...
+        subject_select = getStringArrayPref().toArray(new String[0]);
 
-        /************ListView*******/
-        final ArrayAdapter adapter = new ArrayAdapter(this, R.layout.simple_list_item, subject_select);
-        final ListView listview = (ListView)findViewById(R.id.choose_subjects_List);
+        adapter = new ArrayAdapter(getApplicationContext(), R.layout.simple_list_item, subject_select);
+        listview = (ListView)findViewById(R.id.choose_subjects_List);
         listview.setAdapter(adapter);
-
-
         CustomSelectBtn next_btn = (CustomSelectBtn)findViewById(R.id.choose_subjects_nextbtn);
         CustomSelectBtn back_btn = (CustomSelectBtn)findViewById(R.id.choose_subjects_backbtn);
 
@@ -54,25 +68,29 @@ public class ChooseSubjectsActivity extends AppCompatActivity {
             public void onClick(View view) {
                 SparseBooleanArray checkedItems = listview.getCheckedItemPositions(); //체크박스로 체크한 셀의 정보를 담고 있는 희소 논리 배열 얻어오기
                 int count = adapter.getCount(); //전체 몇개인지 세기
-
+                all_subject = takeAlldata();
                 if(checkedItems.size()!=0){
                     for(int i=count-1; i>=0; i--){
                         if(checkedItems.get(i)){ //희소 논리 배열의 해당 인덱스가 선택되어 있다면
-                            subject.add(subject_select[i]) ; //arrayList에 추가하기
-                            ++index;
-                            Log.d("데이터확인", String.valueOf(checkedItems.get(i)));
+                            subject.add(all_subject.get(i)) ; //arrayList에 추가하기
                         }
-                        Intent intent = new Intent(
-                                getApplicationContext(),
-                                SelectTimeSetActivity.class);
-                        intent.putExtra("subject",subject.toArray()); //체크박스로 선택한 객체만 넘어오게 해야 함
-                        intent.putExtra("Write", Write); //정보 전송 -> 학점(int)
-                        intent.putExtra("Grade", Grade); //정보 전송 -> 몇학년인지(int)
-                        startActivity(intent);
-                        overridePendingTransition(0, 0);
                     }
-                } else {
+
+                } /*else {
                     Toast.makeText(ChooseSubjectsActivity.this, "꼭 필수로 들을 과목을 선택해주세요.", Toast.LENGTH_SHORT).show();
+                }*/
+                if(isnotconflict(subject)){
+                    onSaveData(subject);
+                    Intent intent = new Intent(
+                            getApplicationContext(),
+                            SelectTimeSetActivity.class);
+                    intent.putExtra("Write", Write); //정보 전송 -> 학점(int)
+                    intent.putExtra("Grade", Grade); //정보 전송 -> 몇학년인지(int)
+                    startActivity(intent);
+                    overridePendingTransition(0, 0);
+                }
+                else{
+                    subject = new ArrayList<>();
                 }
                 listview.clearChoices() ;
                 adapter.notifyDataSetChanged();
@@ -93,7 +111,6 @@ public class ChooseSubjectsActivity extends AppCompatActivity {
             }
         });
     }
-
     @Override
     public void onBackPressed() {
         //화면에서 뒤로가기 방지
